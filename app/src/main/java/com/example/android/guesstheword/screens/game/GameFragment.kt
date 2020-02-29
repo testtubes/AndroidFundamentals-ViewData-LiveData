28a8d3966d0e984e9f6082ba1ec252f6093861e5
biwin <biwin007@gmail.com>
2020-02-29 15:24:15 +0530

    updates result display with ViewModelFactory

--- app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
@@ -21,9 +21,11 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
+import androidx.navigation.fragment.NavHostFragment
 import com.example.android.guesstheword.R
 import com.example.android.guesstheword.databinding.GameFragmentBinding
 
@@ -56,6 +58,7 @@ class GameFragment : Fragment() {
 
         binding.correctButton.setOnClickListener { onCorrect() }
         binding.skipButton.setOnClickListener { onSkip() }
+        binding.endGameButton.setOnClickListener { onEndGame() }
         updateScoreText()
         updateWordText()
         return binding.root
@@ -87,5 +90,19 @@ class GameFragment : Fragment() {
         binding.scoreText.text = viewModel.score.toString()
     }
 
+
+    /* end game */
+
+    private fun gameFinished(){
+        Toast.makeText(activity, "Game has just finished", Toast.LENGTH_SHORT).show()
+        val action = GameFragmentDirections.actionGameToScore()
+        action.score = viewModel.score
+        NavHostFragment.findNavController(this).navigate(action)
+
+    }
+
+    private fun onEndGame(){
+        gameFinished()
+    }
 
 }
---
+++ app/src/main/java/com/example/android/guesstheword/screens/score/ScoreViewModel.kt
@@ -0,0 +1,12 @@
+package com.example.android.guesstheword.screens.score
+
+import androidx.lifecycle.ViewModel
+
+class ScoreViewModel(finalScore: Int): ViewModel() {
+
+    var score = finalScore
+
+    init {
+
+    }
+}
